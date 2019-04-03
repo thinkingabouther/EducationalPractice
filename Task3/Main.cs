93df8a6f96fd29f5370aaa7a4230bd201e7227f2
thinkingabouther <neustroev.arseny@gmail.com>
2019-04-04 01:29:43 +0500

    Added solved 3 task and utilities library with ConsoleInputParsing methods [2]

---
+++ Task3/Main.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Utilities;
+
+namespace Task3
+{
+    internal static class MainProgram
+    {
+        public static void Main(string[] args)
+        {
+            double answer = new PointU(
+                ConsoleInputParse.Double(
+                    "Input X value", "Unable to cast to double"),
+                ConsoleInputParse.Double(
+                    "Input Y value", "Unable to cast to double")).GetUValue();
+            Console.WriteLine($"The answer is {answer}");            
+        }
+    }
+
+    class PointU
+    {
+        private readonly double x;
+        private readonly double y;
+        private bool isInAreaD;
+        
+        public PointU(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+            isInAreaD = IsInAreaD();
+        }
+
+        bool IsInAreaD()
+        {
+            return y >= 0 & (x <= 0 & x * x + y * y <= 1 | x >= 0 & x * x + y * y >= 0.09 & x * x + y * y <= 1);
+        }
+
+        public double GetUValue()
+        {
+            if (isInAreaD) return x * x - 1;
+            return Math.Sqrt(Math.Abs(x - 1));
+        }
+    }
+
+
+}
---
+++ Utilities/Main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Xml;
+
+namespace Utilities
+{
+    public static class ConsoleInputParse
+    {
+        public static double Double(string invite, string errorMessage)
+        {
+            bool flag;
+            double output;
+            do
+            {
+                Console.WriteLine(invite);
+                flag = double.TryParse(Console.ReadLine(), out output);
+                if (!flag)
+                {
+                    Console.WriteLine(errorMessage);
+                }
+            } while (!flag);
+
+            return output;
+        }
+    }
+}
